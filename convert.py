534c6327d114e44188ff460b3b0f5a083cc58309
Edward J. Schwartz <eschwartz@cert.org>
2019-11-04 14:07:28 -0500

    Initial working prototype

--- convert.py
+++ convert.py
@@ -5,37 +5,113 @@ from llvmlite import ir
 import sys
 import json
 
-jsonir = json.loads(open(sys.argv[1], "r").read())
+#jsonir = json.loads(open(sys.argv[1], "r").read())
 
 vars = {}
 vertices = {}
+functions = {}
 
 module = ir.Module(name="test")
+
+abort = ir.Function (module, ir.FunctionType (ir.VoidType (), []), "abort")
+
+# Hack
+M = ir.GlobalVariable (module, ir.PointerType (ir.IntType (8)), "M")
 
 def get_builder_op(op, irb):
     exp_map = {
         'add': 'add',
+        'and': 'and_',
         'invert': 'neg',
         'xor': 'xor',
-        'asr': 'ashr'
+        'asr': 'ashr',
+        'umul': 'mul'
     }
     return getattr(irb, exp_map[op])
 
 shifts = ['asr']
-reduction = ['xor']
+reduction = ['xor', 'add']
 
-def convert_vertex (v, func):
-    if v['id'] not in vertices:
-        block = func.append_basic_block (name=v['name'])
-        irb = ir.IRBuilder (block)
-        vertices[v['id']] = convert_stmts (v['stmts'], irb)
+def convert_file (file, module):
+    # Declare stuff
+    for func in file.items ():
+        add_func (func, module)
 
-    return vertices[v['id']]
+    for func in file.items ():
+        convert_func (func, module)
+
+def add_func (func, module):
+    addr = int (func[0])
+    body = func[1]
+
+    assert addr not in functions
+    void = ir.VoidType()
+    fnty = ir.FunctionType(void, [])
+    func = ir.Function(module, fnty, name=str("0x%x" % addr))
+    functions[addr] = func
+
+    # Add starting blocks for each vertex
+    for v in body['vertices']:
+        add_vertexid (v, addr, func)
+
+
+def convert_func (func, module):
+    #print (len(func))
+    #print (func)
+
+    addr = int (func[0])
+    #print (addr)
+    body = func[1]
+
+    llvmfunc = functions[addr]
+
+    for v in body['vertices']:
+        #print (v)
+        convert_vertex (v, addr, body, llvmfunc)
+
+    return func
+
+def add_vertexid (v, funcaddr, llvmfunc):
+    if funcaddr not in vertices:
+        vertices[funcaddr] = {}
+    assert v['id'] not in vertices[funcaddr]
+    block = llvmfunc.append_basic_block (name=v['name'])
+    vertices[funcaddr][v['id']] = block
+    return block
+
+def convert_vertex (v, funcaddr, body, llvmfunc):
+    block = vertices [funcaddr] [v['id']]
+    irb = ir.IRBuilder (block)
+
+    convert_stmts (v['stmts'], irb)
+
+    if not irb.block.is_terminated:
+
+        edges = [e for e in body['edges'] if e['src'] == v['id']]
+
+        if len(edges) == 0:
+            irb.ret_void ()
+        elif len(edges) == 1:
+            e = edges[0]
+            irb.branch (vertices [funcaddr] [e['dst']])
+            #irb.branch (convert_vertexid (e['dst'], llvmfunc))
+        elif len(edges) == 2:
+            cond = convert_exp (edges[0] ['cond'], irb)
+            irb.cbranch (cond,
+                         vertices [funcaddr] [edges[0] ['dst']],
+                         vertices [funcaddr] [edges[1] ['dst']])
+        else:
+            assert False
+
+    return block
 
 def convert_stmts(stmts, irb=ir.IRBuilder ()):
-    return [convert_stmt(stmt, irb) for stmt in stmts if stmt]
+    for stmt in stmts:
+        convert_stmt (stmt, irb)
 
 def convert_stmt(stmt, irb=ir.IRBuilder ()):
+    #print (stmt)
+
     if stmt['op'] == "InsnStmt":
         return None
     elif stmt['op'] == "RegWriteStmt":
@@ -43,23 +119,38 @@ def convert_stmt(stmt, irb=ir.IRBuilder ()):
         e = convert_exp (stmt['exp'], irb)
         return irb.store (e, dest)
     elif stmt['op'] == "MemWriteStmt":
-        mem = None # ???
+        mem = M # ???
         addr = convert_exp (stmt['addr'], irb)
         val = convert_exp (stmt['exp'], irb)
         ptr = irb.inttoptr (irb.add (irb.ptrtoint (mem, addr.type),
                                      addr),
-                            val.type)
+                            val.type.as_pointer ())
         return irb.store (val, ptr)
+    elif stmt['op'] == "CallStmt":
+        if stmt['exp'] ['op'] == "constant":
+            targetaddr = int (stmt['exp'] ['const'], 16)
+            targetfunc = functions [targetaddr]
+            return irb.call (targetfunc, [])
+        else:
+            irb.call (abort, [])
+            return irb.unreachable ()
+            # XXX: Should be able to mark this unreachable but we
+            # can't right now because we try to add the branch which
+            # fails
     else:
         assert False
 
 def convert_var_noload (exp, irb=ir.IRBuilder ()):
+    if exp['varname'] == 'M':
+        return M
+
     if exp['varid'] not in vars:
         if exp['varname'] == 'M':
             typ = ir.PointerType (ir.IntType (8))
         else:
             typ = ir.IntType (int(exp['width']))
-            vars[exp['varid']] = ir.GlobalVariable(module, typ, exp['varname'])
+        varname = exp['varname'] if exp['varname'] != "" else "v%d" % int(exp['varid'])
+        vars[exp['varid']] = ir.GlobalVariable(module, typ, varname)
 
     return vars[exp['varid']]
 
@@ -128,12 +219,13 @@ def convert_exp(exp, irb=ir.IRBuilder ()):
             # We have n > 2 children, but LLVM only wants 2
             return reduce (lambda a, b: buildf (a, b), children)
         else:
-            return buildf (*children)
+            # By specifying a name we detect mismatches in the number of arguments
+            return buildf (*children, name="generic")
 
 #double = ir.DoubleType()
-void = ir.VoidType()
-fnty = ir.FunctionType(void, [])
-func = ir.Function(module, fnty, name="test")
+#void = ir.VoidType()
+#fnty = ir.FunctionType(void, [])
+#func = ir.Function(module, fnty, name="test")
 #block = func.append_basic_block(name="entry")
 
 # exp1 = convert_exp(json.loads(conststr))
@@ -143,7 +235,7 @@ func = ir.Function(module, fnty, name="test")
 # print(exp)
 
 #exp = (convert_exp(json.loads(sys.stdin.read()), irb=ir.IRBuilder(block)))
-convert_vertex(json.loads(sys.stdin.read()), func)
+convert_file(json.loads(sys.stdin.read()), module)
 #print (exp)
 
 print (module)
