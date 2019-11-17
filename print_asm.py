024eee88d92bdda5abf9e40c43720ad35a3920e9
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2019-11-17 14:45:11 -0500

    print expressions

--- print_asm.py
+++ print_asm.py
@@ -3,6 +3,8 @@ from llvmlite import ir, binding
 import fileinput
 import json
 import sys
+import io
+import ctypes
 
 def is_flag(name):
     flags = ['pf_0', 'zf_0', 'af_0', 'cf_0', 'of_0', 'sf_0']
@@ -75,7 +77,8 @@ class AsmPrinter(object):
         var = self.var_str(s['var'])
         if not self._with_flags and is_flag(var):
             return False
-        self.writeln('{} := {}'.format(var, ''))
+        exp = self.exp_str(s['exp'])
+        self.writeln('{} := {}'.format(var, exp))
         return True
 
     def print_CallStmt(self, s):
@@ -84,15 +87,81 @@ class AsmPrinter(object):
 
     def print_MemWriteStmt(self, s):
         addr = self.addr_str(s['addr'])
-        self.writeln("MEM[{}] := {}".format(addr, ''))
+        exp = self.exp_str(s['exp'])
+        self.writeln("MEM[{}] := {}".format(addr, exp))
 
     def addr_str(self, addr):
-        return 'addr'
+        out = io.StringIO()
+        self.print_addr(addr, out)
+        return out.getvalue()
+
+    def print_addr(self, addr, out):
+        assert (addr['type'] == 'exp')
+        op = addr['op']
+        if op == 'variable':
+            out.write(self.var_str(addr))
+        elif op == 'constant':
+            out.write(self.const_str(addr))
+        elif op == 'add':
+            kids = addr['children']
+            assert(len(kids) == 2)
+            self.print_addr(kids[0], out)
+            out.write(' + ')
+            self.print_addr(kids[1], out)
+
     def var_str(self, var):
         if var['varname'] != '':
             return str(var['varname'])
         else:
             return 'v{}'.format(var['varid'])
+
+    def const_str(self, v):
+        val = int(v['const'], 16)
+
+        if v['width'] == '64':
+            val = ctypes.c_int64(val)
+            val = val.value
+        elif v['width'] == '32':
+            val = ctypes.c_int32(val)
+            val = val.value
+        else:
+            pass
+        return str(val)
+
+    def exp_str(self, v):
+        out = io.StringIO()
+        self.print_exp(v, out)
+        return out.getvalue()
+
+    def print_exp(self, v, out):
+        op = v['op']
+        if op == 'variable':
+            out.write(self.var_str(v))
+        elif op == 'constant':
+            out.write(self.const_str(v))
+        elif op == 'add':
+            kids = v['children']
+            assert(len(kids) == 2)
+            self.print_exp(kids[0], out)
+            out.write(' + ')
+            self.print_exp(kids[1], out)
+        elif op == 'read':
+            array, idx = v['children']
+            self.print_exp(array, out)
+            out.write('[')
+            self.print_exp(idx, out)
+            out.write(']')
+        elif op == 'concat':
+            kids = v['children']
+            kids = [self.exp_str(k) for k in kids]
+            out.write(' :: '.join(kids))
+        elif op == 'extract':
+            low, high, body = v['children']
+            self.print_exp(body, out)
+            out.write('[{}:{}]'.format(int(low['const']), int(high['const'])))
+
+        else:
+            out.write(str(v))
 
 program = json.loads("\n".join(f for f in fileinput.input()))
 printer = AsmPrinter(program, sys.stdout)
